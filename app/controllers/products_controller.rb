38b8765f88b4ed63775e159af74d9e40fa333260
TamDN <tamdn@runsystem.net>
2019-04-11 17:03:32 +0700

    add function remove all product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -61,6 +61,12 @@ class ProductsController < ApplicationController
     end
   end
 
+  def remove_all
+    Product.delete_all
+    @products = Product.all
+    render :index
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products do
+    collection do
+      get 'remove_all'
+    end
+  end
   root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
